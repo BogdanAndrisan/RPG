c13c4d94fbb1423aed8414ebed381c4aa31f973d
Tosatsu <bobo1793@gmail.com>
2014-06-23 20:41:20 +0300

    0.0.1.0i
    Att:-level up check, based on exp. Up on HP calculation and EXP/Level
    calc. Will improve them further later on.
    Main calls LevelUp function, will switch to call only on exp
    modification later.

--- Scripts/Attributes.cs
+++ Scripts/Attributes.cs
@@ -5,8 +5,9 @@ public class Attributes{
 	const int baseHP = 100;
 	const int baseMP = 100;
 	const int baseEN = 100;
+	public int statPoints=0;
 	public int level = 1;
-	public int EXP;
+	public int EXP=0;
 	public int HP;
 	public int MP;
 	public int EN;
@@ -14,6 +15,15 @@ public class Attributes{
 	public int agility;
 	public int intelligence;
 	public int getHP(){
-		return HP=baseHP+(int)(level^2)+(int)(strength*5);
+		return HP=baseHP+(int)(level*level)+(int)(strength*5);
+	}
+	public void LevelUp(){
+		if(this.EXP>this.level*90){
+			this.EXP=this.EXP-level*90;
+			this.level++;
+			this.statPoints+=5;
+			this.strength+=2;
+			this.HP=this.getHP();//recalculate HP when leveled.
+		}
 	}
 }
--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -36,6 +36,7 @@ public class Main : MonoBehaviour {
 		
 	}	
 	void Update () {
+		att.LevelUp();
 		SelectEnemy();
 		FireballCheck();
 	}
